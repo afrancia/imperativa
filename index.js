b52112aa225a54fe64798e8f0c9d34eef89715d3
afrancia <anthony.francia.francia@gmail.com>
2023-10-18 19:56:54 -0500

    primeros archivos

---
+++ index.js
@@ -0,0 +1,49 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+
+// let nombre = prompt("Ingrese su nombre: ");
+// console.log(`Hola ${nombre}!!!`);
+
+// let numeroPreferido = 3;
+
+// console.log(numeroPreferido);
+
+// if(true){
+//     let numeroPreferido = 5;
+//     console.log(numeroPreferido);
+// }
+
+const apellido = "Francia";
+console.log(apellido);
+
+if (true) {
+  const apellido = "Segil";
+  console.log(apellido);
+}
+
+let verdadero = true;
+console.log(verdadero);
+
+let falso = false;
+console.log(falso);
+
+//varible del tipo NaN
+
+let notAtNumber = NaN;
+console.log(notAtNumber);
+
+let nulo = null;
+console.log(nulo);
+
+let sinValor;
+console.log(sinValor);
+
+let miComputadora = {
+  marca: "Windows",
+  modelo: 2017,
+  estaEncendida: true,
+};
+
+console.log(miComputadora);
+
+let seriesFavoritas = ["Breaking bad", "Big bang theory"];
+console.log(seriesFavoritas);
